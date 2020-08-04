b1b6d1398081009c77c5793078d2f1ec0cc7a0d2
ogawakan <ogawakan1208@gmail.com>
2020-08-04 09:50:37 +0900

    新規作成画面の表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_03_183718) do
+ActiveRecord::Schema.define(version: 2020_08_03_222734) do
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
